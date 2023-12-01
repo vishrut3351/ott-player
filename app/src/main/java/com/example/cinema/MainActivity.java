413bee13ac6684eb00b366be0b7d44d7a82704fa
vishrut3351 <151846908+vishrut3351@users.noreply.github.com>
2023-12-01 18:55:24 +0530

    ragister form design

--- app/src/main/java/com/example/cinema/MainActivity.java
+++ app/src/main/java/com/example/cinema/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.cardview.widget.CardView;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -19,21 +22,14 @@ public class MainActivity extends AppCompatActivity {
     int month;
     int day;
 
+    String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CardView cardView = findViewById(R.id.cv_continue);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Sign_in.class);
-                startActivity(intent);
-            }
-        });
-
-        EditText editText = findViewById(R.id.birth);
+        EditText editText = findViewById(R.id.ll_birth);
 
         Calendar calendar = Calendar.getInstance();
         editText.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +47,71 @@ public class MainActivity extends AppCompatActivity {
                         }
                     },year,month,day);
                     datePickerDialog.show();
+            }
+        });
+
+        EditText name = findViewById(R.id.ll_name);
+        EditText birth = findViewById(R.id.ll_birth);
+        EditText username = findViewById(R.id.ll_username);
+        EditText email = findViewById(R.id.ll_email);
+        EditText number = findViewById(R.id.ll_number);
+        EditText password = findViewById(R.id.ll_password);
+        EditText confirm = findViewById(R.id.ll_confirm);
+
+        CardView cardView = findViewById(R.id.cv_continue);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean isUser = true;
+
+                if (TextUtils.isEmpty(name.getText().toString())){
+                    isUser = false;
+                    name.setError("Invalid Name");
+                }
+
+                if (TextUtils.isEmpty(birth.getText().toString())){
+                    isUser = false;
+                    Toast.makeText(MainActivity.this, "Empty date", Toast.LENGTH_SHORT).show();
+                }
+
+                if (TextUtils.isEmpty(username.getText().toString())){
+                    isUser = false;
+                    username.setError("Invalid Username");
+                }
+
+                if (TextUtils.isEmpty(number.getText().toString())){
+                    isUser = false;
+                    number.setError("Invalid Number");
+                }
+
+                if (TextUtils.isEmpty(password.getText().toString())){
+                    isUser = false;
+                    password.setError("Invalid Password");
+                }
+
+                if (TextUtils.isEmpty(confirm.getText().toString())){
+                    isUser = false;
+                    confirm.setError("Invalid Confirm Password");
+                }
+
+                if (TextUtils.isEmpty(email.getText().toString())){
+                    isUser = false;
+                    email.setError("Enter your Email");
+                }
+                else {
+                    if (!email.getText().toString().trim().matches(emailpattern)){
+                        isUser = false;
+                        Toast.makeText(MainActivity.this, "Invalid Email!", Toast.LENGTH_SHORT).show();
+                        email.setError("Invalid Email");
+                        Toast.makeText(MainActivity.this,birth.getText().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                if (isUser == true){
+                    Intent intent = new Intent(MainActivity.this, Sign_in.class);
+                    startActivity(intent);
+                }
             }
         });
 
