ac44a6a150a991992959937bb348d27937689279
vishrut3351 <151846908+vishrut3351@users.noreply.github.com>
2023-12-01 17:46:38 +0530

    add datepicker

--- app/src/main/java/com/example/cinema/MainActivity.java
+++ app/src/main/java/com/example/cinema/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.cinema;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    int year;
+    int month;
+    int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView cardView = findViewById(R.id.cv_continue);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Sign_in.class);
+                startActivity(intent);
+            }
+        });
+
+        EditText editText = findViewById(R.id.birth);
+
+        Calendar calendar = Calendar.getInstance();
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    final Calendar c = Calendar.getInstance();
+                    year = calendar.get(Calendar.YEAR);
+                    month = calendar.get(Calendar.MONTH);
+                    day = calendar.get(Calendar.DAY_OF_MONTH);
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                            editText.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                        }
+                    },year,month,day);
+                    datePickerDialog.show();
+            }
+        });
+
     }
 }
